288286157985556a19eedec51c5e5b24df5c8739
ghazalmansour <ghazalmansour@gmail.com>
2020-03-20 11:59:48 -0400

    removed node-moduls

---
+++ mi-home/build/precache-manifest.48e67c2f57349a1ee71538920bf05269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c26512743e3cfcb61a22aa420f8deca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ab43cb4f630db827d9c",
+    "url": "/static/css/main.ef439c68.chunk.css"
+  },
+  {
+    "revision": "efdd0cb33bc06ceb9d09",
+    "url": "/static/js/2.3076cf18.chunk.js"
+  },
+  {
+    "revision": "289c8e00ed6700ae291237a4bdebe674",
+    "url": "/static/js/2.3076cf18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab43cb4f630db827d9c",
+    "url": "/static/js/main.b4d8320d.chunk.js"
+  },
+  {
+    "revision": "66efb538580e138109f7",
+    "url": "/static/js/runtime-main.5bf7e5ae.js"
+  }
+]);
